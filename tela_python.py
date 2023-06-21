21b3090dd64d7be500c25a61ff83f4852c3ba8c3
Marco Maximo <marcomaximojr@gmail.com>
2023-06-21 09:31:09 -0300

    Update tela_python.py
    Update file

--- tela_python.py
+++ tela_python.py
@@ -1,10 +1,11 @@
 from PySimpleGUI import PySimpleGUI as sg
 
+
 #Layout definido
 sg.theme('LightBlue3')
 layout = [
-	[sg.Text('Usuário'), sg.Input(key='usuario')],
-	[sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+	[sg.Text('Usuário'), sg.Input(key='usuario', size=(25, 2))],
+	[sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(25, 2))],
 	[sg.Checkbox('Salvar login?')],
 	[sg.Button('Entrar')]
 ]
@@ -22,3 +23,5 @@ while True:
 	if eventos == 'Entrar':
 		if valores['usuario'] == 'datacoin' and valores['senha'] == 'datacoin':
 			print('Canal Datacoin logado!')
+		else:
+			print('Login não realizado');
